c862f52f7e76797e1d141862de0542cddda345d1
Nathan Schepers <nathan@alsegno.com>
2020-03-27 12:05:13 +0000

    take bucket name from command line

--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -7,5 +7,6 @@ cd _site/
 rm README.md 
 
 # assumes that awscli is installed and appropriate keys are configured to write to this bucket
-aws s3 rm s3://BUCKETNAME --recursive
-aws s3 cp --recursive . s3://BUCKETNAME --grants read=uri=http://acs.amazonaws.com/groups/global/AllUsers
+# provide the appropriate bucket name on the command line
+aws s3 rm s3://$1 --recursive
+aws s3 cp --recursive . s3://$1 --grants read=uri=http://acs.amazonaws.com/groups/global/AllUsers
