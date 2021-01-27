6c89ea2559b07b46ff5168c00cf734a9970b4f64
Nathan <nathan.schepers@protonmail.com>
2021-01-27 11:59:57 +0000

    added pdf generation to deploy script

--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -1,12 +1,25 @@
 #!/bin/bash
 
+today=`date '+%Y%m%d'`
+
 cd ..
-bundle exec jekyll clean && bundle exec jekyll build
+bundle exec jekyll clean
+bundle exec jekyll build
+
+bundle exec jekyll serve &
+serve_pid=$!
+
+sleep 2
+wkhtmltopdf --image-dpi 300 -L 0mm -R 0mm --javascript-delay 2000 http://localhost:4000 ./_site/resume-$today.pdf
+
+kill -2 $serve_pid
+
 cp favicon.ico _site/
-cd _site/
-rm README.md 
+rm _site/README.md
 
 # assumes that awscli is installed and appropriate keys are configured to write to this bucket
 # provide the appropriate bucket name on the command line
+cd _site/
 aws s3 rm s3://$1 --recursive
 aws s3 cp --recursive . s3://$1 --grants read=uri=http://acs.amazonaws.com/groups/global/AllUsers
+
