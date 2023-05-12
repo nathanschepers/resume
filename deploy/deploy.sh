8ad273b729f8aa71e6339b38d3756640f04440e6
Nathan <nathan.schepers@gmail.com>
2023-05-12 11:47:33 +0200

    changes

--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -8,7 +8,7 @@ bundle exec jekyll serve &
 serve_pid=$!
 
 sleep 2
-wkhtmltopdf --image-dpi 300 -L 0mm -R 0mm --javascript-delay 2000 http://localhost:4000 ./_site/resume.pdf
+wkhtmltopdf --lowquality --image-dpi 72 -L 0mm -R 0mm --javascript-delay 2000 http://localhost:4000 ./_site/resume.pdf
 
 kill -2 $serve_pid
 
@@ -17,10 +17,10 @@ rm _site/README.md
 
 # assumes that awscli is installed and appropriate keys are configured to write to this bucket
 # provide the appropriate bucket name on the command line
-cd _site/
-aws s3 rm s3://$1 --recursive
-aws s3 cp --recursive . s3://$1 --grants read=uri=http://acs.amazonaws.com/groups/global/AllUsers
-
-for id in $(aws cloudfront list-distributions --query "DistributionList.Items[*].{id:Id,origin:Origins.Items[0].Id}[?origin=='S3-$1'].id" --output text);
-  do aws cloudfront create-invalidation --distribution-id $id --paths "/*";
-done;
+#cd _site/
+#aws s3 rm s3://$1 --recursive
+#aws s3 cp --recursive . s3://$1 --grants read=uri=http://acs.amazonaws.com/groups/global/AllUsers
+#
+#for id in $(aws cloudfront list-distributions --query "DistributionList.Items[*].{id:Id,origin:Origins.Items[0].Id}[?origin=='S3-$1'].id" --output text);
+#  do aws cloudfront create-invalidation --distribution-id $id --paths "/*";
+#done;
