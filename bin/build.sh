31347f276e4ab21a4fdde55784b84520d7e97b0b
Nathan <nathan.schepers@gmail.com>
2023-09-20 11:42:27 +0200

    wip

--- bin/build.sh
+++ bin/build.sh
@@ -1,8 +1,10 @@
 SRC_DIR=${GITHUB_WORKSPACE}/src
+BIN_DIR=${GITHUB_WORKSPACE}/bin
 STAGE_DIR=${GITHUB_WORKSPACE}/stage
 
+
 # import helpers
-. ./helpers.sh
+. "${BIN_DIR}"/helpers.sh
 
 cd "${SRC_DIR}"
 coloredEcho "Building static site." blue
@@ -13,6 +15,6 @@ SERVE_PID=$!
 sleep 2
 
 coloredEcho "Generating PDFs." blue
-wkhtmltopdf -L 0mm -R 0mm --javascript-delay 2000 http://localhost:4000 "${STAGE_DIR}/resume.pdf"
+"${BIN_DIR}"/wkhtmltopdf -L 0mm -R 0mm --javascript-delay 2000 http://localhost:4000 "${STAGE_DIR}/resume.pdf"
 
 kill -2 ${SERVE_PID}
